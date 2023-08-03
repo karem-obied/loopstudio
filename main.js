562d19e9d4822dad84c1fbeb4d53a7503a4b2896
karem-obied <karemobied12@hotmail.com>
2023-08-03 19:30:42 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let image = document.querySelectorAll(".box");
+let counter = 1;
+image.forEach((e) => {
+  e.style.cssText = `background-image: url(/images/desktop/image-${counter}.jpg);`;
+  counter++;
+});
+let close = document.querySelector(".close");
+let menu = document.querySelector(".hi");
+let ul = document.querySelector("header ul");
+menu.onclick = () => {
+  ul.classList.add("show");
+  close.style.display = "block";
+  menu.style.display = "none";
+  close.onclick = () => {
+    ul.classList.remove("show");
+    close.style.display = "none";
+    menu.style.display = "block";
+  };
+};
